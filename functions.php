3aa3a0f976d7018827ade937c2f9ae34860fe843
Francesco <franc.liuzzi@gmail.com>
2019-01-03 11:24:28 +0100

    final

--- functions.php
+++ functions.php
@@ -1,9 +1,25 @@
 <?php
 
+// Defines
+define( 'FL_CHILD_THEME_DIR', get_stylesheet_directory() );
+define( 'FL_CHILD_THEME_URL', get_stylesheet_directory_uri() );
+
+// Classes
+require_once 'classes/class-fl-child-theme.php';
+
+// Actions
+add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
+//Disable gravity form anchor for a specific form with id5 & id14
+/*add_filter( 'gform_confirmation_anchor_5', '__return_false' );
+add_filter( 'gform_confirmation_anchor_14', '__return_false' );*/
+//Disable gravity form field labels selector
+add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
+
+
 
 add_filter('Forms3rdPartyIntegration_service_filter_args', 'SIDEA_forwardCookies4PardotIntegration', 10, 4);
 
-if (!function_exists('SIDEA_forwardCookies4PardotIntegration')) {
+if (!function_exists('SIDEA_forwardCookies4PardotIntegration')) { // faccio il forward dei cookie e dell'IP dell'utente
     function SIDEA_forwardCookies4PardotIntegration ($post_args) {
         $post_args['cookies']                    = $_COOKIE;
         $post_args['headers']['Forwarded']       = " for=" .$_SERVER['REMOTE_ADDR'];
@@ -17,9 +33,12 @@ add_filter('Forms3rdPartyIntegration_service_filter_post_0', 'SIDEA_replaceOther
 
 if (!function_exists('SIDEA_replaceOtherChoice')) {
     function SIDEA_replaceOtherChoice ($values, $service, $cf7) {
-        if ($values['input_3'] == 'gf_other_choice') {
-            $values['input_3'] = $values['input_3_other'];
+        if ($values['importo'] == 'gf_other_choice') {
+            $values['importo'] = $values['importo_altro']; // valorizzo il campo importo con il valore inserito dall'utente nel campo 'altro'
+        } else {
+            $values['importo'] = str_replace(".", "", $values['importo']); // rimuovo l'eventuale punto all'interno dell'importo
         }
+        unset($values['importo_altro']);
 
         return $values;
     }
