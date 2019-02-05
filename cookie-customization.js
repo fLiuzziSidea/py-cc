7fad5b5c74594bb45d8e4e1a6b8a946704cac71f
Francesco <franc.liuzzi@gmail.com>
2019-02-05 16:43:31 +0100

    fix final

--- cookie-customization.js
+++ cookie-customization.js
@@ -1,9 +1,11 @@
 // CookieCustomizationOptions = {
 //     YES_LABEL = 'Acconsento',
+
 //     NO_LABEL = 'No',
+
 //     PRIVACY_POLICY_LABEL = 'Privacy policy',
 //     PRIVACY_POLICY_URL = '/privacy-policy/',
-    
+
 //     MAX_SECONDS_TRYING = 5,
 //     HIDE_NO_BUTTON = true, // true false
 //     ACCEPT_AFTER_SCROLLING = 0.25 // % of page OR true/false
