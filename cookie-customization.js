e7517a6e94ce36311ebf9810874f6802d83cdbd6
Francesco <franc.liuzzi@gmail.com>
2019-02-05 17:07:38 +0100

    aggiunto init

--- cookie-customization.js
+++ cookie-customization.js
@@ -1,16 +1,3 @@
-// CookieCustomizationOptions = {
-//     YES_LABEL = 'Acconsento',
-
-//     NO_LABEL = 'No',
-
-//     PRIVACY_POLICY_LABEL = 'Privacy policy',
-//     PRIVACY_POLICY_URL = '/privacy-policy/',
-
-//     MAX_SECONDS_TRYING = 5,
-//     HIDE_NO_BUTTON = true, // true false
-//     ACCEPT_AFTER_SCROLLING = 0.25 // % of page OR true/false
-// };
-
 if (jQuery) {
     $ = jQuery;
 
@@ -23,7 +10,7 @@ if (jQuery) {
         MAX_SECONDS_TRYING = 5,
         HIDE_NO_BUTTON = true, // true false
         ACCEPT_AFTER_SCROLLING = 0.25 // % of page OR true/false
-    } = typeof CookieCustomizationOptions !== undefined ? CookieCustomizationOptions : {};
+    } = typeof CookieCustomizationOptions !== 'undefined' ? CookieCustomizationOptions : {};
     
     $(function(){
         let i = 0;
