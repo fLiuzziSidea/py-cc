fb2a29167b0b519fd4b2b0666fea8734427427a1
Francesco Liuzzi <francesco.liuzzi@sideagroup.com>
2019-02-05 16:56:40 +0100

    Update cookie-customization.js

--- cookie-customization.js
+++ cookie-customization.js
@@ -10,7 +10,7 @@ if (jQuery) {
         MAX_SECONDS_TRYING = 5,
         HIDE_NO_BUTTON = true, // true false
         ACCEPT_AFTER_SCROLLING = 0.25 // % of page OR true/false
-    } = typeof CookieCustomizationOptions !== undefined ? CookieCustomizationOptions : {};
+    } = typeof CookieCustomizationOptions !== 'undefined' ? CookieCustomizationOptions : {};
     
     $(function(){
         let i = 0;
